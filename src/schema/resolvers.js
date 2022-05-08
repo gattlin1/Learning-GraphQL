31bd2ab6621950c409c49bebe681dce2b9a2b709
gattlin1 <gattlin1@live.missouristate.edu>
2022-05-08 09:48:51 -0400

    Added resolver for getting specific user and also added for getting all and specific movies

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,9 +1,32 @@
-const { UserList } = require('../FakeData');
+const { UserList, MovieList } = require('../FakeData');
 
 const resolvers = {
   Query: {
     users: () => {
       return UserList;
+    },
+    user: (_, args) => {
+      const id = Number(args.id);
+      for (const user of UserList) {
+        if (user.id === id) return user;
+      }
+    },
+    movies: () => {
+      return MovieList;
+    },
+    movie: (_, args) => {
+      for (const movie of MovieList) {
+        if (movie.name === args.name) return movie;
+      }
+    },
+  },
+
+  User: {
+    favoriteMovies: () => {
+      return MovieList.filter(
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
