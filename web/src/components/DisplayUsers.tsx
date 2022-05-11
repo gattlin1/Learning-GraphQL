153e56374cf57f1ee42ec9688c5cbecd8af25a86
gattlin1 <gattlin1@live.missouristate.edu>
2022-05-11 15:40:47 -0400

    Adding create user mutation request.

--- web/src/components/DisplayUsers.tsx
+++ web/src/components/DisplayUsers.tsx
@@ -1,5 +1,5 @@
-import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import React, { useState } from 'react';
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -13,21 +13,66 @@ const QUERY_ALL_USERS = gql`
   }
 `;
 
+const CREATE_USER = gql`
+  mutation CreateUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      name
+      id
+    }
+  }
+`;
+
 function DisplayUsers() {
-  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  const { data, loading, refetch } = useQuery(QUERY_ALL_USERS);
+  const [createUser, { data: createUserData }] = useMutation(CREATE_USER);
+
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(0);
+  const [nationality, setNationality] = useState('');
+
+  const handleCreateUser = () => {
+    createUser({
+      variables: {
+        input: {
+          name,
+          username,
+          age,
+          nationality,
+        },
+      },
+    });
+    refetch();
+  };
 
   if (loading) {
     return <h1>Data is Loading...</h1>;
   }
-  if (data) {
-    console.log(data);
-  }
-
-  if (error) {
-    console.log(error);
-  }
   return (
     <div>
+      <div>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type='text'
+          placeholder='Name...'
+        />
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          type='text'
+          placeholder='Username...'
+        />
+        <input
+          onChange={(e) => setAge(Number(e.target.value))}
+          type='number'
+          placeholder='Age...'
+        />
+        <input
+          onChange={(e) => setNationality(e.target.value.toUpperCase())}
+          type='text'
+          placeholder='Nationality...'
+        />
+        <button onClick={handleCreateUser}>Create User</button>
+      </div>
       {data &&
         data.users.map((user: any, i: number) => {
           return (
